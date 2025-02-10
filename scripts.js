a43fe261ea36589ffb4ff1ab7e309e8530ba3f4e
Vega Lyrae <150460969+vegalyraevt@users.noreply.github.com>
2025-02-09 23:54:54 -0500

    added kurea and added js for moving pages effects

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const elements = document.querySelectorAll('.fade-in, .fade-in-top-subtle, .fade-in-bottom-subtle, .fade-in-subtle');
+
+    function checkVisibility() {
+        elements.forEach((element, index) => {
+            const rect = element.getBoundingClientRect();
+            if (rect.top < window.innerHeight * 1.2 && rect.bottom > 0) { // Adjusted to 120% of the viewport height
+                setTimeout(() => {
+                    element.classList.add('visible');
+                }, index * 100); // Delay each element by 100ms
+            }
+        });
+    }
+
+    window.addEventListener('scroll', checkVisibility);
+    checkVisibility(); // Initial check
+});
