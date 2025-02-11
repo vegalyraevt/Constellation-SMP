386f5aa6ca8b97167e69e73b8acf934e1efe079a
Vega Lyrae <150460969+vegalyraevt@users.noreply.github.com>
2025-02-11 12:17:58 -0500

    added version lock and fixed headers for bio pages

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles.css';
+
+// Add any JavaScript code you need for your project here
+console.log('Welcome to Constellation SMP');
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production', // Set mode to production
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
